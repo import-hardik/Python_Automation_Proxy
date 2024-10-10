8da093b4eca9fa81730efc613a06517f79723a27
Hardik <hardik.57v@gmail.com>
2024-10-10 22:14:15 +0530

    Update proxy.py
    code update

--- proxy.py
+++ proxy.py
@@ -1,5 +1,9 @@
 import subprocess
-
+#     hello if you are reading this kindly follow me on Github 
+#     https://github.com/import-hardik/
+#     https://www.linkedin.com/in/hardik-verma-6b9a06257/
+#     https://www.instagram.com/import_hardik/
+#     Hardik Verma Logging out
 def get_ssid():
     try:
         # Use the nmcli command to get the SSID of the active connection
@@ -25,9 +29,10 @@ def run_command(command):
             
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running the command: {e}")
+        print("Is it really a Linux Machine")
 
 
-# Print the SSID
+# compair SSID
 if get_ssid()=="HCST_Mathura":
     run_command("gsettings set org.gnome.system.proxy mode 'manual'")
 else:
